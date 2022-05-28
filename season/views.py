ebcd42ed51f02507addce3e811cf019148ba9008
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-05-28 20:41:21 -0300

    adicionado imagens de temporada

--- season/views.py
+++ season/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def listar_season(request):
-    return render(request, 'season/listar-season.html')
+    seasons = [
+        {"year": "2022","name": "Outono", "img": "season-autumn.jpg"},
+        {"year": "2022", "name": "Primáveira", "img": "season-spring.jpg"},
+        {"year": "2022", "name": "Verão", "img": "season-summer.jpg"},
+        {"year": "2022", "name": "Inverno", "img": "season-winter.jpg"},
+        {"year": "2021","name": "Outono", "img": "season-autumn.jpg"},
+        {"year": "2021", "name": "Primáveira", "img": "season-spring.jpg"},
+        {"year": "2021", "name": "Verão", "img": "season-summer.jpg"},
+        {"year": "2021", "name": "Inverno", "img": "season-winter.jpg"},
+    ]
+    return render(request, 'season/listar-season.html',{"seasons": seasons})
 
 
 def editar_season(request, id):
