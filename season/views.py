803a398942d526ee051c83653c144e5e230addf3
janderson-code <jbgfilebarbosa@gmail.com>
2022-05-28 21:38:57 -0300

    Update Views da season, correção ortográfica
    Primavéira => Primavera

--- season/views.py
+++ season/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 def listar_season(request):
     seasons = [
-        {"year": "2022","name": "Outono", "img": "season-autumn.jpg"},
-        {"year": "2022", "name": "Primáveira", "img": "season-spring.jpg"},
+        {"year": "2022", "name": "Outono", "img": "season-autumn.jpg"},
+        {"year": "2022", "name": "Primavera", "img": "season-spring.jpg"},
         {"year": "2022", "name": "Verão", "img": "season-summer.jpg"},
         {"year": "2022", "name": "Inverno", "img": "season-winter.jpg"},
-        {"year": "2021","name": "Outono", "img": "season-autumn.jpg"},
-        {"year": "2021", "name": "Primáveira", "img": "season-spring.jpg"},
+        {"year": "2021", "name": "Outono", "img": "season-autumn.jpg"},
+        {"year": "2021", "name": "Primavera", "img": "season-spring.jpg"},
         {"year": "2021", "name": "Verão", "img": "season-summer.jpg"},
         {"year": "2021", "name": "Inverno", "img": "season-winter.jpg"},
     ]
-    return render(request, 'season/listar-season.html',{"seasons": seasons})
+    return render(request, 'season/listar-season.html', {"seasons": seasons})
 
 
 def editar_season(request, id):
