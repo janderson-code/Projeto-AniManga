4b95064381990486d5275a90b4e5dd714d96206d
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2022-05-31 23:21:13 -0300

    movido templates e forms de animanga para main app

--- animanga/views.py
+++ animanga/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm, AuthenticationFormCustom
+from ..main.forms import NewUserForm, AuthenticationFormCustom
 from django.contrib.auth import login as user_login, authenticate, logout as user_logout  # add this
 from django.contrib import messages
 import requests
@@ -7,144 +7,144 @@ import random
 
 
 def home(request):
-	return render(request, 'home.html', {
-		'animes': {
-			'most_popular': popular_titles(5, "anime"),
-			'not_release': future_release_titles(4, "anime")
-		},
-		'mangas': {
-			'best_rating': best_rating_titles(4, "manga")
-		}
-	})
+    return render(request, 'home.html', {
+        'animes': {
+            'most_popular': popular_titles(5, "anime"),
+            'not_release': future_release_titles(4, "anime")
+        },
+        'mangas': {
+            'best_rating': best_rating_titles(4, "manga")
+        }
+    })
 
 
 def login(request):
 
-	def auth_user(form):
-		username = form.cleaned_data.get('username')
-		password = form.cleaned_data.get('password')
-		return authenticate(username=username, password=password)
-	if request.user.is_authenticated:
-		return redirect('home')
-	if request.method != "POST":
-		return render(request, "login.html", {'login_form': AuthenticationFormCustom()})
-	form = AuthenticationFormCustom(request, data=request.POST)
-	if not form.is_valid():
-		print(form)
-		messages.error(request, "Usuário ou senha incorretos.")
-		return render(request, "login.html", {'login_form': form})
-	user = auth_user(form)
-	if user is not None:
-		user_login(request, user)
-		return redirect("home")
-	else:
-		return render(request, "login.html", {"login_form": form})
+    def auth_user(form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        return authenticate(username=username, password=password)
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method != "POST":
+        return render(request, "login.html", {'login_form': AuthenticationFormCustom()})
+    form = AuthenticationFormCustom(request, data=request.POST)
+    if not form.is_valid():
+        print(form)
+        messages.error(request, "Usuário ou senha incorretos.")
+        return render(request, "login.html", {'login_form': form})
+    user = auth_user(form)
+    if user is not None:
+        user_login(request, user)
+        return redirect("home")
+    else:
+        return render(request, "login.html", {"login_form": form})
 
 
 def register(request):
-	if request.method != "POST":
-		return render(request, "register.html", {'register_form': NewUserForm()})
+    if request.method != "POST":
+        return render(request, "register.html", {'register_form': NewUserForm()})
 
-	form = NewUserForm(request.POST)
-	
-	if not form.is_valid():
-		messages.error(request, "Registração falhou. Por favor, tente novamente.")
-		return render(request, "register.html", {'register_form': form})
-	
-	user = form.save()
-	user_login(request, user)
-	return redirect("home")
+    form = NewUserForm(request.POST)
 
+    if not form.is_valid():
+        messages.error(
+            request, "Registração falhou. Por favor, tente novamente.")
+        return render(request, "register.html", {'register_form': form})
+
+    user = form.save()
+    user_login(request, user)
+    return redirect("home")
 
 
 def logout(request):
-	user_logout(request)
-	return redirect('login')
+    user_logout(request)
+    return redirect('login')
 
 
 def get_title_image(images, imageType, desiredSize):
 
-	imagesSize = ['large', 'original', 'medium', 'tiny']
-	if imageType == 'cover':
-		imagesTypes = ['coverImage', 'posterImage']
-	elif imageType == 'poster':
-		imagesTypes = ['posterImage', 'coverImage']
+    imagesSize = ['large', 'original', 'medium', 'tiny']
+    if imageType == 'cover':
+        imagesTypes = ['coverImage', 'posterImage']
+    elif imageType == 'poster':
+        imagesTypes = ['posterImage', 'coverImage']
 
-	for imageType in imagesTypes:
-		if not images[imageType]:
-			continue
-		if images[imageType][desiredSize]:
-			return images[imageType][desiredSize]
-		for imageSize in imagesSize:
-			if imagesSize == desiredSize or not images[imageType][imageSize]:
-				continue
-			return images[imageType][imageSize]
+    for imageType in imagesTypes:
+        if not images[imageType]:
+            continue
+        if images[imageType][desiredSize]:
+            return images[imageType][desiredSize]
+        for imageSize in imagesSize:
+            if imagesSize == desiredSize or not images[imageType][imageSize]:
+                continue
+            return images[imageType][imageSize]
 
 
 def get_base_title(attr):
-	title = {
-		'name': attr['canonicalTitle'],
-		'synopsis': attr['synopsis'],
-		'coverImage':  get_title_image(attr, 'cover', 'large'),
-		'posterImage': get_title_image(attr, 'poster', 'medium'),
-		'abbreviatedTitles': attr['abbreviatedTitles'],
-		'averageRating': attr['averageRating'],
-	}
-	return title
+    title = {
+        'name': attr['canonicalTitle'],
+        'synopsis': attr['synopsis'],
+        'coverImage':  get_title_image(attr, 'cover', 'large'),
+        'posterImage': get_title_image(attr, 'poster', 'medium'),
+        'abbreviatedTitles': attr['abbreviatedTitles'],
+        'averageRating': attr['averageRating'],
+    }
+    return title
 
 
 def get_anime(data):
-	attr = data['attributes']
-	anime = get_base_title(attr)
-	anime['episodeCount'] = attr['episodeCount']
-	return anime
+    attr = data['attributes']
+    anime = get_base_title(attr)
+    anime['episodeCount'] = attr['episodeCount']
+    return anime
 
 
 def get_manga(data):
-	attr = data['attributes']
-	manga = get_base_title(attr)
-	manga["chapterCount"] = attr['chapterCount']
-	return manga
+    attr = data['attributes']
+    manga = get_base_title(attr)
+    manga["chapterCount"] = attr['chapterCount']
+    return manga
 
 
 def get_random_title(titles, total):
-	return random.sample(titles, total)
+    return random.sample(titles, total)
 
 # Will create a url and request -> https://kitsu.io/api/edge/anime?sort=-userCount&page[limit]=20
 
 
 def request(type, parameters):
-	url = "https://kitsu.io/api/edge/{0}?".format(type)
-	for i, (key, value) in enumerate(parameters.items()):
-		url += "{0}={1}".format(key, value)
-		if i < len(parameters)-1:
-			url += "&"
-	print(url)
-	return requests.get(url).json()
+    url = "https://kitsu.io/api/edge/{0}?".format(type)
+    for i, (key, value) in enumerate(parameters.items()):
+        url += "{0}={1}".format(key, value)
+        if i < len(parameters)-1:
+            url += "&"
+    print(url)
+    return requests.get(url).json()
 
 
 def popular_titles(total, type, offset=30):
-	response = request(type, {
-		'sort': '-userCount', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
-	titles = []
-	for data in response['data']:
-		titles.append(get_anime(data) if type == 'anime' else get_manga(data))
-	return get_random_title(titles, total)
+    response = request(type, {
+        'sort': '-userCount', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
+    titles = []
+    for data in response['data']:
+        titles.append(get_anime(data) if type == 'anime' else get_manga(data))
+    return get_random_title(titles, total)
 
 
 def future_release_titles(total, type, offset=30):
-	response = request(type, {
-		'sort': '-startDate', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
-	titles = []
-	for data in response['data']:
-		titles.append(get_anime(data) if type == 'anime' else get_manga(data))
-	return get_random_title(titles, total)
+    response = request(type, {
+        'sort': '-startDate', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
+    titles = []
+    for data in response['data']:
+        titles.append(get_anime(data) if type == 'anime' else get_manga(data))
+    return get_random_title(titles, total)
 
 
 def best_rating_titles(total, type, offset=50):
-	response = request(type, {
-		'sort': 'ratingRank', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
-	titles = []
-	for data in response['data']:
-		titles.append(get_anime(data) if type == 'anime' else get_manga(data))
-	return get_random_title(titles, total)
+    response = request(type, {
+        'sort': 'ratingRank', 'page[limit]': total, 'page[offset]': random.randint(0, offset)})
+    titles = []
+    for data in response['data']:
+        titles.append(get_anime(data) if type == 'anime' else get_manga(data))
+    return get_random_title(titles, total)
---
+++ main/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class AuthenticationFormCustom(AuthenticationForm):
+    username = forms.CharField(label="Usuário", error_messages={
+                               'required': 'Esse campo não deve estar vazio.'})
+    password = forms.CharField(label="Senha", error_messages={
+                               'required': 'Esse campo não deve estar vazio.'}, widget=forms.PasswordInput)
+    error_messages = {
+        'invalid_login': ("Por favor, insira um nome de usuário e senha válidos. Note que ambos os campos diferenciam maiúsculas e minúsculas."),
+    }
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class NewUserForm(UserCreationForm):
+
+    error_messages = {
+        'password_mismatch': ("As senhas não são iguais."),
+    }
+    email = forms.EmailField(required=True, label='E-mail', error_messages={
+        'required': 'Esse campo não deve estar vazio.',
+                    'invalid': 'Insira um e-mail válido.',
+                    'unique': 'Esse e-mail já está em uso.'
+    })
+    username = forms.CharField(label="Usuário", error_messages={
+        'unique': ("Esse nome de usuário já existe. Por favor, escolha outro."),
+        'required': ("Esse campo não deve estar vazio."),
+        'invalid': ("Por favor, insira um nome de usuário válido."),
+    })
+    password1 = forms.CharField(label="Senha", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Senha Confirmação", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
